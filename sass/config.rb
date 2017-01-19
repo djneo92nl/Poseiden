f18b4cb3ce638852d5fd2c14f954f00a8cad8e55
remko janse <remko@djneo.nl>
2017-01-19 14:08:02 +0100

    Updating Styling

---
+++ sass/config.rb
@@ -0,0 +1,7 @@
+http_path = "/"  
+sass_dir = '../src/Style'
+css_dir = '../webroot/css'
+images_dir = '../webroot/img'
+javascripts_dir = '../webroot/js'
+environment = :development
+output_style = :compressed
