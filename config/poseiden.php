eadc5fbe4bced14612846d65d4d146c460e2ffe3
remko janse <remko@djneo.nl>
2017-01-20 16:03:56 +0100

    Updated Device Configuration

--- config/poseiden.php
+++ config/poseiden.php
@@ -1,7 +1,7 @@
 <?php
 return [
     'Poseiden' => [
-        'interfaces' => [
+        'deviceConnector' => [
 	        'PhilipsHueBridge' => [
 		        'name' => 'Hue Bridge',
 		        'brand' => 'Philips',
@@ -9,8 +9,26 @@ return [
 	        ]
         ],
 	    'devices' => [
-
-	    ]
+            ''
+	    ],
+        'deviceTypes' => [
+            'simpleDevice' => [
+                'name' => 'Simple on off device',
+                'functions' => [
+                    'getState', 'setState', 'setOff', 'setOn'
+                ],
+                'interfaceName' => 'simpleDevice'
+            ],
+            'dimmableDevice' => [
+                'name' => 'dimmable Device',
+                'description' => 'dimmable Device or light',
+                'dimStep' => '256',
+                'functions' => [
+                    'getState', 'setState', 'setOff', 'setOn'
+                ],
+                'interfaceName' => 'dimmableDevice'
+            ]
+        ]
     ],
 
 ];
