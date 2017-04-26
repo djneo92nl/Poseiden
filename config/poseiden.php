bc1b10cdc1ec6be504fb34f9080552298d59d538
remco janse <r.janse@youwe.nl>
2017-04-26 12:02:09 +0200

    [TASK] Added some config

--- config/poseiden.php
+++ config/poseiden.php
@@ -5,25 +5,40 @@ return [
 	        'PhilipsHueBridge' => [
 		        'name' => 'Hue Bridge',
 		        'brand' => 'Philips',
+                'author' => 'Poseiden',
+                'version' => '0.0.1',
                 'device discovery' =>  true,
 		        'class' => 'App\Utility\PhilipsHue\DeviceConnector\BridgeConnector'
 	        ],
             'simpleMQTTConnector' => [
                 'name' => 'Simple MQTT Connector',
                 'brand' => 'Poseiden',
-                'device discovery' => false,
+                'author' => 'Poseiden',
+                'version' => '0.0.1',
+                'devicediscovery' => false,
                 'class' => 'App\Utility\Poseiden\DeviceConnector\simpleMqttConnector'
+            ],
+            'yeeLightConnector' => [
+                'name' => 'YeeLight Connector',
+                'brand' => 'Xiaomi',
+                'author' => 'Poseiden',
+                'version' => '0.0.1',
+                'devicediscovery' => true,
+                'class' => 'App\Utility\Poseiden\DeviceConnector\yeeLightConnector'
             ]
         ],
 	    'devices' => [
             'h801RGBBeta' => [
                 'name' => 'H801 RGB MQTT',
                 'brand' => 'Poseiden',
-                'version' => '0.0.1'
+                'version' => '0.0.1',
+                'type' => 'colorlight',
+                'deviceConnector' => 'simpleMQTTConnector',
+                'class' => 'App\Utility\Poseiden\Devices\h801RGBBeta'
             ]
 	    ],
         'deviceTypes' => [
-            'simpleDevice' => [
+            'simple Device' => [
                 'name' => 'Simple on off device',
                 'functions' => [
                     'getState', 'setState', 'setOff', 'setOn'
@@ -31,13 +46,22 @@ return [
                 'interfaceName' => 'simpleDevice'
             ],
             'dimmableDevice' => [
-                'name' => 'dimmable Device',
+                'name' => 'Dimmable Device',
                 'description' => 'dimmable Device or light',
                 'dimStep' => '255',
                 'functions' => [
                     'getState', 'setState', 'setOff', 'setOn'
                 ],
                 'interfaceName' => 'dimmableDevice'
+            ],
+            'colorlight' => [
+                'name' => 'Color Light',
+                'description' => 'Colored Light',
+                'dimStep' => '255',
+                'functions' => [
+                    'getState', 'setState', 'setOff', 'setOn', 'setColor'
+                ],
+                'interfaceName' => 'colorlight'
             ]
         ]
     ],
